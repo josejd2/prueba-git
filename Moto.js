25bbd219f204150a5fcd72629f9fa60b38b11866
josejd2 <josejd2.jlv@gmail.com>
2025-03-04 12:17:19 +0100

    Creo una clase llamada Moto que su padre es Vehiculo

---
+++ Moto.js
@@ -0,0 +1,14 @@
+import Vehiculo from "./Vehiculo";
+
+class Moto extends Vehiculo{
+    constructor(marca, modelo, cilindrada){
+        super(marca, modelo)
+        this.cilindrada = cilindrada
+    }
+
+    describir(){
+        return `Esta moto es una ${this.marca} modelo ${this.modelo} con ${this.cilindrada}cc.`;
+    }
+}
+
+export default Moto;
