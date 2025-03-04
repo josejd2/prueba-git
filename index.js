d997063e2892016e67d4f83290d8df14ab26fafe
josejd2 <josejd2.jlv@gmail.com>
2025-03-04 13:09:41 +0100

    Hago un get a /saludo/:nombre

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
-
 app.get('/', (req, res) =>{
     res.send('Servidor funcionando')
-})
+});
+
+app.get('/saludo/:nombre', (req, res) =>{
+    const nombre = req.params.nombre;
+    res.send(`Hola, ${nombre}`)
+});
 
 app.listen(PORT,() =>{
     console.log(`Servidor lanzado en http://localhost:${PORT}`)
-})
+});
